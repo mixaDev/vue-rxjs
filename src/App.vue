3a6e2498e05d9cd3bfaf06b2c944ad0de5a24378
Михайло Голубовський <lamerumiata@gmail.com>
2017-07-28 17:39:05 +0300

    use rxjs

---
+++ src/App.vue
@@ -0,0 +1,33 @@
+<template>
+  <div id="app">
+    <span>Count: {{ count }}</span>
+  </div>
+</template>
+
+<script>
+//  import { Observable } from 'rxjs/Rx'
+  import { Observable } from 'rxjs/Observable'
+  import 'rxjs/add/observable/interval'
+  import 'rxjs/add/operator/filter'
+
+  export default {
+    name: 'app',
+    data () {
+      return {
+        count: 0
+      }
+    },
+    created () {
+      const obs = Observable.interval(1000)
+      obs
+        .filter((value) => value % 2 == 0)
+        .subscribe(
+        (value) => this.count = value
+      )
+    }
+  }
+</script>
+
+<style>
+
+</style>
